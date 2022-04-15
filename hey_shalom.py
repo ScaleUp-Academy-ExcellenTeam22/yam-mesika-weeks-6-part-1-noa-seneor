ee1a96c7aeeb0cdc586066d118c4ca84b7d9a9aa
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-04-15 17:17:55 +0200

    added hey_shalom

---
+++ hey_shalom.py
@@ -0,0 +1,11 @@
+def words_length(sentence):
+    """
+
+    :param sentence: string
+    :return: list containing length of every words in sentence
+    """
+    return [len(word) for word in sentence.split()]
+
+
+if __name__ == '__main__':
+    print(words_length("Toto, I've a feeling we're not in Kansas anymore"))
