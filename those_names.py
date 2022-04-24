dfbe7f71615ecb3e058912109253afac383cb063
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-04-24 11:57:41 +0200

    Update those_names.py

--- those_names.py
+++ those_names.py
@@ -1,15 +1,17 @@
-import string
 from itertools import product
 
 
 def full_names(first, last, min_length=0):
     """
+    Function that receives a list of first names and a list of last names
+    and return all unique combinations of firstnames and lastnames in a list
     :param first: list of first names
     :param last: list of last names
     :param min_length: (optional) minimum length of name combination
-    :return: return all unique combinations of firstnames and lastnames
+    :return: combination list
     """
-    return [f[0].upper() + f[1::] + ' ' + l[0].upper() + l[1::] for f, l in list(product(first, last)) if (len(f) + len(l) >= min_length -1)]
+    return [firstnames[0].upper() + firstnames[1::] + ' ' + lastnames[0].upper() + lastnames[1::]
+            for firstnames, lastnames in list(product(first, last)) if (len(firstnames) + len(lastnames) >= min_length -1)]
 
 
 first_names = ['avi', 'moshe', 'yaakov']
