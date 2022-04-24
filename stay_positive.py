1378296edfe79ac4e8a62e12c6574447d71ba932
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-04-24 12:11:44 +0200

    Update stay_positive.py

--- stay_positive.py
+++ stay_positive.py
@@ -1,10 +1,9 @@
 def get_positive_numbers():
     """
-    Function that ask the user to enter a list of number
-    separated with comma
-    :return: list of only positive number from the list
+    Function that request as input a list of numbers separated with a comma
+    :return: List of only positive number from the list
     """
-    input_list = input("enter number list separated with comma")
+    input_list = input("Enter number list separated with comma")
     lst = list(map(lambda x: int(x), input_list.split(',')))
     positive_list = list(filter(lambda x: x > 0, lst))
     return positive_list
